dd2b59ffc796d2315b390e008050ce72a8ae92a8
OnurKonuk174 <51912619+OnurKonuk174@users.noreply.github.com>
2022-10-05 19:14:30 +0300

    Initial commit

---
+++ src/dairehesap/Main.java
@@ -0,0 +1,22 @@
+package dairehesap;
+
+public class Main {
+
+	public static void main(String[] args) {
+	   double yariCap=4;
+	   double pi=3.16;
+	   double daireAlan=0;
+	   double daireCevre=0;
+	   daireAlan=pi*yariCap*yariCap;
+	   System.out.println("Daire'nin Alanı="+daireAlanı);
+	   daireCevre=2*pi*yariCap;
+	   System.out.println("Daire'nin Çevresi="+daireCevre);
+	   
+	
+		
+		  
+        
+
+	}
+
+}
